7ba40d08c48175b44f4dd9a7c2577e3d5cbfd03c
Axmedov Yoqub <axmedov.yoqub0522@gmail.com>
2026-01-05 14:01:40 +0500

    Add entry point for running the application

--- main.py
+++ main.py
@@ -51,5 +51,9 @@ def usd_rates():
         "updated_at": datetime.now().isoformat(),
         "banks": scrape_usd_rates()
     }
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
 #http://127.0.0.1:8000/api/usd
  #uvicorn main:app --reload
